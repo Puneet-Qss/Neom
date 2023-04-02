f3f611b07bc20527b469f725c03921a282585995
Puneet-Qss <129063347+Puneet-Qss@users.noreply.github.com>
2023-04-02 14:08:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
+
+
+  // Internet  
+
+function myFunction2() {
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show2");
+  }
